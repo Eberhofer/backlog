6709d2f3bb7e7507aa8c29f5e6d715a49c1f3208
Eberhofer <eberhofer@gmx.ch>
2015-11-13 07:12:17 +0100

    hook up simplified items to UI

--- app.js
+++ app.js
@@ -1,14 +1,160 @@
 'use strict';
 var express = require('express');
 var routes = require('./routes/index');
+var path = require('path');
 var port = process.env.PORT || 3000;
+var pg = require('pg');
+var knex = require('./config').knex;
+var connectionString = require('./config').connectionString;
 
 var app = express();
-app.use('/', routes);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 //server.listen(port, function () {
 //  console.log('Server running on port %d', port);
 //});
+
+app.post('/api/v1/items', function(req, res) {
+
+    var results = [];
+
+    // Grab data from http request
+    var data = {item: req.body.item, item: req.body.story, item: req.body.project_id};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO items(item, story, project_id, complete) values($1, $2)", [data.item, data.story, data.project_id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+});
+
+app.get('/api/v1/items', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+});
+
+app.put('/api/v1/items/:item_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.item_id;
+
+    // Grab data from http request
+    var data = {item: req.body.item, story: req.body.story, project_id: req.body.project_id};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Update Data
+        client.query("UPDATE items SET item=($1), story=($2), project_id=($3) WHERE id=($4)", [data.item, data.story, data.project_id, id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
+});
+
+app.delete('/api/v1/items/:item_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
+
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Delete Data
+        client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
+});
+
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
--- public/scripts/items.js
+++ public/scripts/items.js
@@ -1,35 +1,76 @@
-var CommentBox = React.createClass({
+var ContentBox = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-      <h1>Comments</h1>
-      <CommentList />
-      <CommentForm />
+      <div className="contentBox">
+      <h1>Content</h1>
+      <ContentList data={this.props.data} />
+      <ContentForm />
       </div>
     );
   }
 });
 // tutorial2.js
-var CommentList = React.createClass({
+var ContentList = React.createClass({
   render: function() {
+    var contentNodes = this.props.data.map(function(items) {
+      return (
+        <Content item={items.item} key={items.id}>
+          {items.story}
+        </Content>
+      );
+    });
     return (
-      <div className="commentList">
-        Hello, world! I am a CommentList.
+      <div className="contentList">
+      {contentNodes}
       </div>
     );
   }
 });
 
-var CommentForm = React.createClass({
+var ContentForm = React.createClass({
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
+      <div className="contentForm">
+        Hello, world! I am a ContentForm.
       </div>
     );
   }
 });
+var Content = React.createClass({
+  render: function() {
+    return (
+      <div className="Mycontent">
+        <h2 className="contentItem">
+          {this.props.item}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+var data = [
+  {id: 1, item: "Pete Hunt", story: "This is one comment", project_id: 1},
+  {id: 2, item: "Jordan Walke", sotry: "This is *another* comment", project_id: 1}
+];
 ReactDOM.render(
-  <CommentBox />,
+  <ContentBox data={data}/>,
   document.getElementById('content')
 );
+// <script type='text/jsx'>
+  // var Example1 = React.createClass({
+  //
+  //   add: function() {
+  //     console.log('add 1!');
+  //   },
+  //
+  //   render: function() {
+  //     return(
+  //       <div>
+  //         <h1>Counter</h1>
+  //         <button onClick={this.add}>+</button>
+  //       </div>
+  //     );
+  //   }
+  // });
+  //
+  // React.render(<Example1 />, document.getElementById('content'));
