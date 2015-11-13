0ff0186d068e83853222a9cd554f250b0dcfab16
Eberhofer <eberhofer@gmx.ch>
2015-11-13 14:49:43 +0100

    cosmetics...

--- app.js
+++ app.js
@@ -15,18 +15,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/api/v1/items', function(req, res) {
-
+//curl -H "Content-Type: application/json" -X POST –d "{\"item\":\"cmd api\",\"story\": \"entered through commandline and api\"}" http://localhost:3000/api/v1/items
     var results = [];
-
     // Grab data from http request
     var data = {item: req.body.item, item: req.body.story, item: req.body.project_id};
-
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
-
         // SQL Query > Insert Data
-        client.query("INSERT INTO items(item, story, project_id, complete) values($1, $2)", [data.item, data.story, data.project_id]);
-
+        client.query("INSERT INTO items(item, story, project_id) values($1, $2, $3)", [data.item, data.story, data.project_id]);
         // SQL Query > Select Data
         var query = client.query("SELECT * FROM items ORDER BY id ASC");
 
@@ -79,24 +75,19 @@ app.put('/api/v1/items/:item_id', function(req, res) {
 
     // Grab data from the URL parameters
     var id = req.params.item_id;
-
     // Grab data from http request
     var data = {item: req.body.item, story: req.body.story, project_id: req.body.project_id};
-
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
 
         // SQL Query > Update Data
         client.query("UPDATE items SET item=($1), story=($2), project_id=($3) WHERE id=($4)", [data.item, data.story, data.project_id, id]);
-
         // SQL Query > Select Data
         var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
         // Stream results back one row at a time
         query.on('row', function(row) {
             results.push(row);
         });
-
         // After all data is returned, close connection and return results
         query.on('end', function() {
             client.end();
@@ -107,18 +98,13 @@ app.put('/api/v1/items/:item_id', function(req, res) {
         if(err) {
           console.log(err);
         }
-
     });
-
 });
 
 app.delete('/api/v1/items/:item_id', function(req, res) {
-
     var results = [];
-
     // Grab data from the URL parameters
     var id = req.params.todo_id;
-
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -144,9 +130,7 @@ app.delete('/api/v1/items/:item_id', function(req, res) {
         if(err) {
           console.log(err);
         }
-
     });
-
 });
 
 app.listen(app.get('port'), function() {
